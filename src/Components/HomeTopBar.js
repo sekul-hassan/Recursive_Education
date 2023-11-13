0e255e4b9ea88c969c85e1980b017ced60fa4607
sekul <sekulhassan3031@gmail.com>
2023-11-13 23:00:16 +0600

    Work is started ny navBar and homeTop

--- src/Components/HomeTopBar.js
+++ src/Components/HomeTopBar.js
@@ -10,6 +10,7 @@ function HomeTopBar(props) {
                         <div className="homeTopBackgroundOverlay">
                             <div className="topBarContent">
                                 <h4>hii there have a slider</h4>
+                                <h4>Slider is boom boom</h4>
                             </div>
                         </div>
                     </Col>
