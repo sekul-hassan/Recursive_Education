b9ab2e3ef014e01acff5b12da00d852da5f191ed
sekul <sekulhassan3031@gmail.com>
2023-11-20 01:39:46 +0600

    add Practice Bar

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,27 +18,27 @@ function Footer(props) {
             <Row>
                <Col className="justify-content-start" lg={4} md={4} sm={12}>
                    <h4 className="globalTitle">Contact</h4>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faFacebook}/> Facebook</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faYoutube}/> YouTube</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faInstagram}/> Instagram</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faEnvelope}/> E-mail</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faGithub}/> Github</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faFacebook}/> Facebook</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faYoutube}/> YouTube</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faInstagram}/> Instagram</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faEnvelope}/> E-mail</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faGithub}/> Github</a><br/>
                </Col>
                <Col className="justify-content-start" lg={4} md={4} sm={12}>
                    <h4 className="globalTitle">Important</h4>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faCode}/> Practice</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faBriefcase}/> Projects</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faHome}/> Home</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faInfoCircle}/> About</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faTools}/> Service</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faCode}/> Practice</a><br/>
+                   <a className="contactLink" href="#aa"><FontAwesomeIcon icon={faBriefcase}/> Projects</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faHome}/> Home</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faInfoCircle}/> About</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faTools}/> Service</a><br/>
                </Col>
                <Col className="justify-content-start" lg={4} md={4} sm={12}>
                    <h4 className="globalTitle">Pages</h4>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faLock}/> Privacy Policy</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faScroll}/> Terms & Condition</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faMoneyBill}/> Refund Policy</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faUser}/> Community Guidelines</a><br/>
-                   <a className="contactLink" href=""><FontAwesomeIcon icon={faPhone}/> Contact</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faLock}/> Privacy Policy</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faScroll}/> Terms & Condition</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faMoneyBill}/> Refund Policy</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faUser}/> Community Guidelines</a><br/>
+                   <a className="contactLink" href="#a"><FontAwesomeIcon icon={faPhone}/> Contact</a><br/>
                </Col>
             </Row>
         </Container>
