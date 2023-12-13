e714fe8590d6df8f6209cc895167030eedc2c893
sekul hassan <sekulhassan3031@gmail.com>
2023-12-13 21:38:44 +0600

    adding responsive

--- src/Components/PracticeSideBar.js
+++ src/Components/PracticeSideBar.js
@@ -16,30 +16,43 @@ function PracticeSideBar(props) {
                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleMenu} />
                <Navbar.Collapse id="basic-navbar-nav" className={`${showMenu ? 'show' : ''}`}>
                    <Nav className="flex-column sidebar">
-                       <h4 className="practiceTitle">Javascript</h4>
-                       <Nav.Link href="#link"><Link to="/slider">Slider</Link></Nav.Link>
-                       <Nav.Link href="#anotherlink">Another Link</Nav.Link>
-                       <Nav.Link href="#morelinks">More Links</Nav.Link>
-                       <Nav.Link href="#evenmorelinks">Even More Links</Nav.Link>
-                       <Nav.Link href="#lastlink">Last Link</Nav.Link>
-                       <Nav.Link href="#home">Home</Nav.Link>
-                       <Nav.Link href="#link">Link</Nav.Link>
-                       <Nav.Link href="#anotherlink">Another Link</Nav.Link>
-                       <Nav.Link href="#morelinks">More Links</Nav.Link>
-                       <Nav.Link href="#evenmorelinks">Even More Links</Nav.Link>
-                       <Nav.Link href="#lastlink">Last Link</Nav.Link>
-                       <Nav.Link href="#home">Home</Nav.Link>
-                       <Nav.Link href="#link">Link</Nav.Link>
-                       <Nav.Link href="#anotherlink">Another Link</Nav.Link>
-                       <Nav.Link href="#morelinks">More Links</Nav.Link>
-                       <Nav.Link href="#evenmorelinks">Even More Links</Nav.Link>
-                       <Nav.Link href="#lastlink">Last Link</Nav.Link>
-                       <Nav.Link href="#home">Home</Nav.Link>
-                       <Nav.Link href="#link">Link</Nav.Link>
-                       <Nav.Link href="#anotherlink">Another Link</Nav.Link>
-                       <Nav.Link href="#morelinks">More Links</Nav.Link>
-                       <Nav.Link href="#evenmorelinks">Even More Links</Nav.Link>
-                       <Nav.Link href="#lastlink">Last Link</Nav.Link>
+                       <h4 className="practiceTitle mt-2">Javascript</h4>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
+                       <Link to="/slider">Slider</Link>
                    </Nav>
                </Navbar.Collapse>
            </Navbar>
