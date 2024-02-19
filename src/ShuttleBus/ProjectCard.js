34d7ad799cd063b0b89621c71f75a924a8552e86
sekul hassan <sekulhassan3031@gmail.com>
2024-02-19 15:55:52 +0600

    project card running

--- src/ShuttleBus/ProjectCard.js
+++ src/ShuttleBus/ProjectCard.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import {Card, CardBody, Col, Container, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
+import project1 from "../Assets/Images/ProjectImg/need2.png"
 
 function ProjectCard(props) {
     return (
         <Container fluid="true" className="mt-3">
             <Row className="mx-0">
                 <Col md={4} lg={3} sm={12}>
-                    <Card>
-
+                    <Card className="projectBody">
+                        <img className="projectImg" src={project1} alt={project1}/>
+                       <div className="d-flex my-2">
+                           <Button className="btn-outline-info">Preview</Button>
+                           <Button className="btn-outline-info mx-4">Live Code</Button>
+                       </div>
                     </Card>
                 </Col>
             </Row>
