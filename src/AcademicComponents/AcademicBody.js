a983521245bc282ad98196f79203729fb39ca93c
sekul hassan <sekulhassan3031@gmail.com>
2024-02-26 13:00:52 +0600

    Academic navbar completed

---
+++ src/AcademicComponents/AcademicBody.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+function AcademicBody(props) {
+    return (
+        <Container fluid="ture">
+
+        </Container>
+    );
+}
+
+export default AcademicBody;
+
--- src/AcademicComponents/AcademicNav.js
+++ src/AcademicComponents/AcademicNav.js
@@ -5,13 +5,13 @@ function AcademicNav(props) {
     return (
         <Container fluid="true" className="mt-1">
             <ul className="hslider mx-0 px-4">
-                <li className="mx-0"><a href="">Six</a></li>
-                <li className="mx-2"><a href="">Seven</a></li>
-                <li className="mx-2"><a href="">Eight</a></li>
-                <li className="mx-2"><a href="">Nine</a></li>
-                <li className="mx-2"><a href="">Ten</a></li>
-                <li className="mx-2"><a href="">Eleven</a></li>
-                <li className="mx-0"><a href="">Twelve</a></li>
+                <li className="mx-0"><a href="#abc">Six</a></li>
+                <li className="mx-2"><a href="#abc">Seven</a></li>
+                <li className="mx-2"><a href="#abc">Eight</a></li>
+                <li className="mx-2"><a href="#abc">Nine</a></li>
+                <li className="mx-2"><a href="#abc">Ten</a></li>
+                <li className="mx-2"><a href="#abc">Eleven</a></li>
+                <li className="mx-0"><a href="#abc">Twelve</a></li>
             </ul>
         </Container>
     );
