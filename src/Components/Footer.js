c3aa9506120da350c84744830d4c718f909d32de
sekul hassan <sekulhassan3031@gmail.com>
2024-03-25 10:13:42 +0600

    navbar css fix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,21 +17,21 @@ function Footer() {
         <Container fluid="true" className="px-3 mt-3 copyRight">
             <Row className="mx-0">
                 <Col className="justify-content-start" lg={4} md={4} sm={4}>
-                    <h4 className="title text-dark mt-sm-2">Contact</h4>
+                    <h4 className="globalTitle text-dark mt-sm-2">Contact</h4>
                     <a className="contactLink" href="#a"><FontAwesomeIcon icon={faFacebook}/> Facebook</a><br/>
                     <a className="contactLink" href="#a"><FontAwesomeIcon icon={faYoutube}/> YouTube</a><br/>
                     <a className="contactLink" href="#a"><FontAwesomeIcon icon={faInstagram}/> Instagram</a><br/>
                     <a className="contactLink" href="#a"><FontAwesomeIcon icon={faGithub}/> Github</a><br/>
                 </Col>
                 <Col className="justify-content-start " lg={4} md={4} sm={4}>
-                    <h4 className="title text-dark mt-sm-2">Important</h4>
+                    <h4 className="globalTitle text-dark mt-sm-2">Important</h4>
                     <Link to="/javascript" className="contactLink"><FontAwesomeIcon icon={faCode}/> Practice</Link><br/>
                     <Link to="/project" className="contactLink"><FontAwesomeIcon icon={faBriefcase}/> Projects</Link><br/>
                     <Link to="/about" className="contactLink"><FontAwesomeIcon icon={faInfoCircle}/> About</Link><br/>
                     <Link to="/services" className="contactLink"><FontAwesomeIcon icon={faTools}/> Service</Link><br/>
                 </Col>
                 <Col className="justify-content-start mt-sm-2" lg={4} md={4} sm={4}>
-                    <h4 className="title text-dark">Pages</h4>
+                    <h4 className="globalTitle text-dark">Pages</h4>
                     <Link to="/privacy" className="contactLink" href="#a"><FontAwesomeIcon icon={faLock}/> Privacy Policy</Link><br/>
                     <a className="contactLink" href="#a"><FontAwesomeIcon icon={faScroll}/> Terms & Condition</a><br/>
                     <a className="contactLink" href="#a"><FontAwesomeIcon icon={faMoneyBill}/> Refund Policy</a><br/>
--- src/Components/TopMenu.js
+++ src/Components/TopMenu.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState, useRef } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import white from '../Assets/Images/white.png';
 import black from '../Assets/Images/black.png';
@@ -14,20 +14,6 @@ function TopMenu(props) {
         logo: black,
     });
 
-    const menuRef = useRef(null);
-
-    const closeMenuOnClickOutside = (event) => {
-        if (
-            menuRef.current &&
-            !menuRef.current.contains(event.target) &&
-            document.querySelector('.navbar-collapse.show')
-        ) {
-            const navbarToggler = document.querySelector('.navbar-toggler');
-            if (navbarToggler && !navbarToggler.classList.contains('collapsed')) {
-                navbarToggler.click();
-            }
-        }
-    };
 
     useEffect(() => {
         function handleScroll() {
@@ -55,27 +41,20 @@ function TopMenu(props) {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
-    useEffect(() => {
-        document.addEventListener('mousedown', closeMenuOnClickOutside);
-        return () => {
-            document.removeEventListener('mousedown', closeMenuOnClickOutside);
-        };
-    }, []);
-
     return (
         <Fragment>
-            <Navbar className={css.navBar} collapseOnSelect expand="md" sticky="top" ref={menuRef}>
+            <Navbar className={css.navBar} collapseOnSelect expand="md" sticky="top">
                 <Navbar.Brand className="m-0 p-0" href="#home"><Link to="/"><img className="logoImg" src={css.logo} alt="" /></Link></Navbar.Brand>
                 <Navbar.Toggle className={css.smallNav} aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto" />
                     <Nav className={css.nav}>
-                        <Nav.Link><Link to="/" className={css.navLink}>Home</Link></Nav.Link>
-                        <Nav.Link><Link to="/about" className={css.navLink}>About</Link></Nav.Link>
-                        <Nav.Link><Link to="/services" className={css.navLink}>Service</Link></Nav.Link>
-                        <Nav.Link><Link to="/academic" className={css.navLink}>Academic</Link></Nav.Link>
-                        <Nav.Link><Link to="/project" className={css.navLink}>Projects</Link></Nav.Link>
-                        <Nav.Link><Link to="/javascript" className={css.navLink}>Practice</Link></Nav.Link>
+                        <Link to="/" className={css.navLink}>Home</Link>
+                        <Link to="/about" className={css.navLink}>About</Link>
+                        <Link to="/services" className={css.navLink}>Service</Link>
+                        <Link to="/academic" className={css.navLink}>Academic</Link>
+                        <Link to="/project" className={css.navLink}>Projects</Link>
+                        <Link to="/javascript" className={css.navLink}>Practice</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
