a03792072d93ba8601d39796da9a7a47cb4e789c
sekul <sekulhassan3031@gmail.com>
2024-07-06 01:18:34 +0600

    git link solve

--- src/Components/VideoContextSection.js
+++ src/Components/VideoContextSection.js
@@ -8,8 +8,7 @@ function VideoContextSection({title,description,videoLink,gitHubLink}) {
             <Row className="mx-0">
                 <Col md={6} sm={12} lg={6} className="px-0  mt-3 mxHeight">
                     <ReactPlayer url={videoLink} controls width="100%" height="100%" maxHeight="400px" style={{borderRadius:"10px"}}/>
-                    <h4 className="globalDescription">Git_Hub : <a href={gitHubLink} target="_blank" className="text-info text-decoration-none"
-                           rel="noreferrer"> {gitHubLink}</a></h4>
+
                 </Col>
                 <Col className="px-0  mt-3" md={6} sm={12} lg={6}>
                     <div className="px-3">
@@ -17,6 +16,10 @@ function VideoContextSection({title,description,videoLink,gitHubLink}) {
                         <h5 className="globalDescription">{description}</h5>
                     </div>
                 </Col>
+            </Row>
+            <Row className="mx-0">
+                <h4 className="globalDescription">Git_Hub :
+                    <a href={gitHubLink} target="_blank" className="text-info text-decoration-none" rel="noreferrer"> {gitHubLink}</a></h4>
             </Row>
         </Container>
     );
