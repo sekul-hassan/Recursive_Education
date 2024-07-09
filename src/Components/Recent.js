5841dac1d125adc5ad0b98fe2e16e9b8f814e711
sekul <sekulhassan3031@gmail.com>
2024-07-10 01:27:47 +0600

    update recent activity

--- src/Components/Recent.js
+++ src/Components/Recent.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import practiceLists from "../PracticePages/PracticeConfig";
+import React, {Fragment} from 'react';
 import VideoContextSection from "./VideoContextSection";
 import {Container, Row} from "react-bootstrap";
+import projects from "../ProjectPages/ProjectConfig";
 
 function Recent(props) {
-    const topFour = practiceLists[0].slice(-4);
+    const topFour = projects[0].classes.slice(2,4);
 
     return (
        <Container fluid="true" className="mt-3">
            <h4 className="globalTitle text-dark text-center mbNeg">Recent Activity</h4>
            <Row className="mx-0">
                {topFour && topFour.map((practice, idx) => (
-                   <VideoContextSection videoLink={practice.videoContent.videoLink} gitHubLink={practice.videoContent.gitHubLink} description={practice.videoContent.description} title={practice.videoContent.title}/>
+                   <Fragment>
+                       {
+                           practice.videoContent ? (
+                               <VideoContextSection videoLink={practice.videoContent.videoLink} gitHubLink={practice.videoContent.gitHubLink} description={practice.videoContent.description} title={practice.videoContent.title}/>
+                           ):(
+                               <VideoContextSection videoLink={practice.lecture.videoLink} gitHubLink={practice.lecture.githubLink} description={practice.lecture.description} title={practice.lecture.title}/>
+                           )
+                       }
+
+                   </Fragment>
                ))}
            </Row>
        </Container>
